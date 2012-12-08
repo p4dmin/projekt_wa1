b2b7b5fe361e19b9c007596109cd5af65a276dba
Thang Do <dpadmin@seznam.cz>
2012-12-08 12:12:15 +0100

    2

---
+++ cenik.php
@@ -0,0 +1,41 @@
+﻿<html>
+<head>
+<meta charset="UTF-8">
+<link rel="stylesheet" type="text/css" href="css/scss/style.css" media="all">
+<title>EURO Nails</title>
+</head>
+<body>
+<div id="all">
+
+	<div class="align_box">
+		<div id="cenik">
+		<p class="headtitle">Ceník služeb</p>
+		<div class="col_left">
+		<h3>Manikúra</h3>
+		<hr class="salon_line">
+		<ul>
+		<li><strong>Manikúra klasické</strong> <em>180,- Kč</em></li>
+		<li><strong>Manikúra klasické</strong> <em>180,- Kč</em></li>
+		<li><strong>Manikúra klasické</strong> <em>180,- Kč</em></li>
+		</ul>
+			<h3>Manikúra</h3>
+		<hr class="salon_line">
+		<ul>
+		<li><strong>Manikúra klasické</strong> <em>180,- Kč</em></li>
+		<li><strong>Manikúra klasické</strong> <em>180,- Kč</em></li>
+		<li><strong>Manikúra klasické</strong> <em>180,- Kč</em></li>
+		</ul>
+		</div>
+		<div class="col_right">
+		<h3>Manikúra</h3>
+		<hr class="salon_line">
+		</div>
+		
+	</div>
+
+
+	
+	
+</div>
+</body>
+</html>
--- kontakt.php
+++ kontakt.php
@@ -10,32 +10,48 @@
 	<div class="align_box">
 		<p class="headtitle">Kontakt</p>
 		<div class="kontakt_col_l"> 
+			<div class="cont_kontakt">
 			<h3>Jednatel</h3>
-			<p> Pan Thuyen Do
-			</p>
-			<p>+420 602 169 997
-			</p>
-			<p>euronails[at]seznam.cz
-			</p>
-			<h3>Dotazy</h3>
-			<p>info[at]euronails.org
-			<p>
-			<h3>Napište nám</h3>
-			<form>
-			<label>Jméno:<input type="text"></label> <br>
-			<label>Email:<input type="text"></label> <br>
-			<label for="captcha">Opište text z obrázku:</label> <br>
-			<input type="text" id="captcha"> <br>
-			<label for="user_text">Vaše sdělení:</label><br>
-			<input type="text" id="user_text"><br>
-			<input type="button" value="Odeslat">
+				<p> Pan Thuyen Do
+				</p>
+				<p>+420 602 169 997
+				</p>
+				<p>euronails[at]seznam.cz
+				</p>
+			</div>
+			
+			<div class="cont_kontakt">
+				<h3>Dotazy</h3>
+				<p>info[at]euronails.org
+				</p>
+			</div>
+			<h3 class="pen_icon">Napište nám</h3>
+			<form id="kontakt_form">
+				<label class="personal_label" for="name">Jméno:</label><input type="text" id="name" class="personal_input" size="30" name="name">
+				<span class="must_star">*</span>
+				<label class="personal_label" for="email">Email:</label><input type="text" id="email" class="personal_input" size="30" name="email"> 
+				<span class="must_star">*</span>
+				<label for="captcha" id="captcha_label">Opište text z obrázku:</label> 
+				<img src="images/captcha/001.png" alt="captcha_image" size="10">
+				<input type="text" id="captcha" name="captcha"> <span class="must_star">*</span>
+				<span class="must_fill">* povinné údaje</span>
+				<label for="user_text">Vaše sdělení:</label>
+				<textarea rows="10" cols="30" id="user_text" name="massage">
+				</textarea>
+				<input type="button" value="Odeslat" class="submit_button">
 			</form>
 
 		</div>
 		<div class="kontakt_col_r">
 			<h3>Sídlo společnosti</h3>
+				<a href="#"><img id="company_house" src="images/company_house.png"></a>
 			<h3>EURO Nails s.r.o.</h3>
+				<p>IČO: 27868648</p>
+				<p>Pražská 3/5
+				</p>
+				<p>268 01 Hořovice</p>
 		</div>
+		
 	</div>
 
 
